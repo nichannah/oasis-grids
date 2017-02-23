6f4bfbbd84b860241a5e97cc431fc0d78e733874
Nicholas Hannah <nicjhannah@gmail.com>
2017-02-23 16:40:13 +1100

    Update OASIS tests to try doing 0.1 deg remap.

--- test/test_oasis.py
+++ test/test_oasis.py
@@ -5,9 +5,75 @@ import shlex
 import glob
 import numpy as np
 import subprocess as sp
+import netCDF4 as nc
 
 from helpers import setup_test_input_dir
 from helpers import calc_regridding_err
+
+def build_oasis(oasis_dir):
+
+        oasis3mct_dir = os.path.join(oasis_dir, 'oasis3-mct')
+
+        # First build oasis3-mct library.
+        ret = sp.call(['make', '-C', oasis3mct_dir, 'ubuntu'])
+        assert ret == 0
+
+        # Build Fortran test code.
+        ret = sp.call(['make', '-C', oasis_dir, 'clean'])
+        assert ret == 0
+        ret = sp.call(['make', '-C', oasis_dir])
+        assert ret == 0
+
+
+def remap_to_mom(oasis_dir, input_dir, mom_hgrid, mom_mask):
+
+        # Make oasis grids
+        my_dir = os.path.dirname(os.path.realpath(__file__))
+        cmd = [os.path.join(my_dir, '../', 'oasisgrids.py')]
+
+        output_grids = os.path.join(oasis_dir, 'grids.nc')
+        output_areas = os.path.join(oasis_dir, 'areas.nc')
+        output_masks = os.path.join(oasis_dir, 'masks.nc')
+
+        mom_args = ['--model_hgrid', mom_hgrid, '--model_mask', mom_mask,
+                    '--grids', output_grids, '--areas', output_areas,
+                    '--masks', output_masks, 'MOM']
+        ret = sp.call(cmd + mom_args)
+        assert ret == 0
+
+        core2_hgrid = os.path.join(input_dir, 't_10.0001.nc')
+        core2_args = ['--model_hgrid', core2_hgrid,
+                      '--grids', output_grids, '--areas', output_areas,
+                      '--masks', output_masks, 'CORE2']
+        ret = sp.call(cmd + core2_args)
+        assert ret == 0
+
+        # Build models
+        build_oasis(oasis_dir)
+
+        # Run model, exchanging a single field
+        cur_dir = os.getcwd()
+        os.chdir(oasis_dir)
+
+        cmd = 'mpirun -np 1 model_src : -np 1 model_dest'
+        t0 = time.time()
+        ret = sp.call(shlex.split(cmd))
+        t1 = time.time()
+        assert ret == 0
+
+        os.chdir(cur_dir)
+
+        # Look at the output of the field.
+        weights = os.path.join(oasis_dir,
+                               'rmp_cort_to_momt_CONSERV_FRACNNEI.nc')
+
+        with nc.Dataset(os.path.join(oasis_dir, 'src_field.nc')) as f:
+            src = f.variables['Array'][:]
+        with nc.Dataset(os.path.join(oasis_dir, 'dest_field.nc')) as f:
+            dest = f.variables['Array'][:]
+
+        return src, dest, weights, (t1 - t0)
+
 
 class TestOasis():
     """
@@ -33,15 +99,7 @@ class TestOasis():
         Build example Fortran code.
         """
 
-        # First build oasis3-mct library.
-        ret = sp.call(['make', '-C', oasis3mct_dir, 'ubuntu'])
-        assert ret == 0
-
-        # Build Fortran test code.
-        ret = sp.call(['make', '-C', oasis_dir, 'clean'])
-        assert ret == 0
-        ret = sp.call(['make', '-C', oasis_dir])
-        assert ret == 0
+        build_oasis(oasis_dir)
 
     @pytest.mark.conservation
     def test_remap_one_deg(self, input_dir, oasis_dir):
@@ -57,57 +115,41 @@ class TestOasis():
             except FileNotFoundError as e:
                 pass
 
-        # Make oasis grids
-        my_dir = os.path.dirname(os.path.realpath(__file__))
-        cmd = [os.path.join(my_dir, '../', 'oasisgrids.py')]
-
-        output_grids = os.path.join(oasis_dir, 'grids.nc')
-        output_areas = os.path.join(oasis_dir, 'areas.nc')
-        output_masks = os.path.join(oasis_dir, 'masks.nc')
-
         mom_hgrid = os.path.join(input_dir, 'grid_spec.nc')
         mom_mask = os.path.join(input_dir, 'grid_spec.nc')
-        mom_args = ['--model_hgrid', mom_hgrid, '--model_mask', mom_mask,
-                    '--grids', output_grids, '--areas', output_areas,
-                    '--masks', output_masks, 'MOM']
-        ret = sp.call(cmd + mom_args)
-        assert ret == 0
 
-        core2_hgrid = os.path.join(input_dir, 't_10.0001.nc')
-        core2_args = ['--model_hgrid', core2_hgrid,
-                      '--grids', output_grids, '--areas', output_areas,
-                      '--masks', output_masks, 'CORE2']
-        ret = sp.call(cmd + core2_args)
-        assert ret == 0
-
-        # Build models
-        self.test_build(oasis_dir)
-
-        # Run model, exchanging a single field
-        cur_dir = os.getcwd()
-        os.chdir(oasis_dir)
-
-        cmd = 'mpirun -np 1 model_src : -np 1 model_dest'
-        t0 = time.time()
-        ret = sp.call(shlex.split(cmd))
-        t1 = time.time()
-        assert ret == 0
-
-        os.chdir(cur_dir)
-
-        # Look at the output of the field.
-        weights = os.path.join(oasis_dir,
-                               'rmp_cort_to_momt_CONSERV_FRACNNEI.nc')
-
-        with nc.Dataset(os.path.join(oasis_dir, 'src_field.nc')) as f:
-            src = f.variables['Array'][:]
-        with nc.Dataset(os.path.join(oasis_dir, 'dest_field.nc')) as f:
-            dest = f.variables['Array'][:]
-
+        src, dest, weights, runtime = remap_to_mom(oasis_dir, input_dir,
+                                                   mom_hgrid, mom_mask)
         rel_err = calc_regridding_err(weights, src, dest)
 
         print('OASIS relative error {}'.format(rel_err))
-        print('OASIS time to make weights and remap one field {}'.format(t1-t0))
+        print('OASIS time to make weights and remap one field {}'.format(runtime))
 
-        assert np.allclose(src_tot, dest_tot, rtol=1e-9)
+        assert rel_err < 1e-9
 
+    @pytest.mark.conservation
+    @pytest.mark.big_ram
+    def test_remap_tenth_deg(self, input_dir, oasis_dir):
+        """
+        Use OASIS for a one degree remapping.
+        """
+
+        # Delete all netcdf files in oasis dir this will include the OASIS
+        # configuration.
+        for f in glob.glob(oasis_dir + '/*.nc'):
+            try:
+                os.remove(f)
+            except FileNotFoundError as e:
+                pass
+
+        mom_hgrid = os.path.join(input_dir, 'ocean_01_hgrid.nc')
+        mom_mask = os.path.join(input_dir, 'ocean_01_mask.nc')
+
+        src, dest, weights, runtime = remap_to_mom(oasis_dir, input_dir,
+                                                   mom_hgrid, mom_mask)
+        rel_err = calc_regridding_err(weights, src, dest)
+
+        print('OASIS relative error {}'.format(rel_err))
+        print('OASIS time to make weights and remap one field {}'.format(runtime))
+
+        assert rel_err < 1e-9
