f363cdc934cae592ca192db82360ef46868caaf9
Nicholas Hannah <nicjhannah@gmail.com>
2017-03-20 13:33:47 +1100

    Add timing to oasis tests.

--- test/oasis/atm.F90
+++ test/oasis/atm.F90
@@ -10,18 +10,24 @@ implicit none
     type(couple_field_type), dimension(:), allocatable :: fields
     integer :: i, j, t, timestep
     logical :: debug
+    real :: time_start, time_finish
 
     debug = .false.
     timestep = 1
 
     ! Initialise the coupler.
+    call cpu_time(time_start)
     call coupler_init('atmxxx', 192, 94, 1, 1)
 
     ! Create/add the coupling field.
     allocate(fields(1))
     call coupler_add_field(fields(1), 'src_field', COUPLER_OUT)
     call coupler_init_done()
+    call cpu_time(time_finish)
 
+    print*, "Startup time: ", time_finish - time_start
+
+    call cpu_time(time_start)
     if (debug) then
       call coupler_dump_field(fields(1), 'src_field.nc')
     endif
@@ -33,6 +39,9 @@ implicit none
 
     call coupler_destroy_field(fields(1))
     call coupler_close()
+    call cpu_time(time_finish)
+
+    print*, "Runtime: ", time_finish - time_start
 
     deallocate(fields)
 
